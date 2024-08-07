2a0634e9b6ca2dfbaa38dc8603596f0cf8deecde
rinjhasijapati <aastha_sijapati@yahoo.com>
2024-08-07 20:28:18 +0545

    create and store in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Notes from './pages/Notes';
 import CreateNote from './pages/CreateNote';
 import EditNote from './pages/EditNote';
+//import dummyNotes from './dummy_notes';
+
+import { useState } from 'react';
 
 const App = () => {
+
+    const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('notes')) || []);
+    
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes))
+    }, [notes])
+
     return (
         <main id='app'>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Notes />} />
-                    <Route path='/create-note' element={<CreateNote />} />
+                    <Route path='/' element={<Notes notes={notes} />} />
+                    <Route path='/create-note' element={<CreateNote setNotes={setNotes} />} />
                     <Route path='/edit-note/:id' element={<EditNote />} />
                 </Routes>
             </BrowserRouter>
--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { IoIosArrowBack } from 'react-icons/io';
+import {v4 as uuid} from 'uuid';
 
-const CreateNote = () => {
+import useCreateDate from '../components/useCreateDate';
+
+const CreateNote = ({setNotes}) => {
+
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const date = useCreateDate();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(title && details) {
+      const note = {id: uuid(), title, details, date}
+      setNotes(prevNotes => [note, ...prevNotes])
+      
+      //redirect to home page
+      navigate('/')
+    }
+  }
+
   return (
     <section>
       <header className='create-note_header'>
         <Link to="/" className="btn"><IoIosArrowBack /></Link>
-        <button className='btn lg primary'>Save</button>
+        <button className='btn lg primary' onClick={handleSubmit}>Save</button>
       </header>
-      <form className='create-note_form'>
-        <input type='text' placeholder='Title' autoFocus />
-        <textarea rows="28" placeholder='Note details...'></textarea>
+      <form className='create-note_form' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} autoFocus />
+        <textarea rows="28" placeholder='Note details...' value={details} onChange={(e) => setDetails(e.target.value)}></textarea>
       </form>
     </section>
   )
